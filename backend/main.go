313c67d54254b84fe0a58481c7ca709ddc892c76
vanekt <vanekt@yandex.ru>
2025-07-04 08:15:48 -0300

    cors settings, env.examples

--- backend/main.go
+++ backend/main.go
@@ -4,16 +4,31 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
+	allowedOrigin := os.Getenv("ALLOWED_ORIGIN")
+
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		if allowedOrigin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", allowedOrigin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		}
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
 		log.Println("🏓 Ping...")
 		time.Sleep(3 * time.Second)
 		log.Println("🏓 Pong")
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{"result": "pong"})
 	})
+
 	http.ListenAndServe(":8080", nil)
 }
